50821acbd0ca8fa84bc4b22b171ee4afc492a97a
Brent Morris <inkrypto@gmail.com>
2018-09-16 18:31:04 -0700

    Update casino.py

--- casino.py
+++ casino.py
@@ -8,8 +8,8 @@ def bet():
 
 	bet = input("How much do you want to bet on Red or Black? ")
 	red_or_black = input("How many bets do you want to make? ")		#num tries on or red/black
-	total = bet * (2 ** red_or_black)								#formula = x(2^y)
-	odds = 37														#odds of winning 37 to 1
+	total = bet * (2 ** red_or_black)					#formula = x(2^y)
+	odds = 37								#odds of winning 37 to 1
 	total_winnings = total * odds
 
 	print('A winning ${} bet on red or black {} times, would yield ${}'.format(bet, red_or_black, total))
